c045a598cd9c0466d853280247ad527dafaede92
Tomas Balaz <tomasba@amdocs.com>
2022-05-16 14:27:26 +0200

    Example how Chain Function with andThen method

--- src/main/java/functionalinterface/_Function.java
+++ src/main/java/functionalinterface/_Function.java
@@ -13,9 +13,18 @@ public class _Function {
 
         Integer increment2 = incrementByOneFunction.apply(1);
         System.out.println(increment2);
+
+        Integer multiply = multiplyBy10Function.apply(increment2);
+        System.out.println(multiply);
+
+        Function<Integer, Integer> addBy1AndThenMultiplyBy10
+                = incrementByOneFunction.andThen(multiplyBy10Function);
+        System.out.println(addBy1AndThenMultiplyBy10.apply(2));
     }
 
    static  Function<Integer, Integer> incrementByOneFunction = number -> number + 1;
+
+    static Function<Integer, Integer> multiplyBy10Function = number -> number * 10;
 
     static int incrementByOne(int number) {
         return number + 1;

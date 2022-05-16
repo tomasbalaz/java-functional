32c5dc9bffe8c92eb78eb2f4e0006ce204606c8b
Tomas Balaz <tomasba@amdocs.com>
2022-05-16 14:14:20 +0200

    Example how to use java.util.function.Function

---
+++ src/main/java/functionalinterface/_Function.java
@@ -0,0 +1,23 @@
+package functionalinterface;
+
+import java.util.function.Function;
+
+public class _Function {
+
+    public static void main(String[] args) {
+//        int apply = integerFunction.apply(1);
+//        System.out.println(apply);
+
+        int increment = incrementByOne(1);
+        System.out.println(increment);
+
+        Integer increment2 = incrementByOneFunction.apply(1);
+        System.out.println(increment2);
+    }
+
+   static  Function<Integer, Integer> incrementByOneFunction = number -> number + 1;
+
+    static int incrementByOne(int number) {
+        return number + 1;
+    }
+}

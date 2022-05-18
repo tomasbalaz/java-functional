12a0debff940f88bb6e5df4f7d85600435ed45ab
Tomas Balaz <tomasba@amdocs.com>
2022-05-18 09:03:25 +0200

    Example how to use java.util.Optional

---
+++ src/main/java/optional/Main.java
@@ -0,0 +1,35 @@
+package optional;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Object defaultValue = Optional.ofNullable(null)
+                .orElseGet(() -> "default value");
+
+        System.out.println(defaultValue);
+
+        String value = Optional.ofNullable("Hello")
+                .orElseGet(() -> "default value");
+
+        System.out.println(value);
+
+        Optional.ofNullable("john@gmail.com")
+                .ifPresent(email -> System.out.println("Sending email to " + email));
+
+        Optional.ofNullable(null)
+                .ifPresentOrElse(
+                        email -> System.out.println("Sending email to " + email),
+                        () -> {
+                            System.out.println("Can not send email");
+                        });
+
+        Supplier<IllegalStateException> exception = () -> new IllegalStateException("exception");
+        Object exceptionValue = Optional.ofNullable(null)
+                .orElseThrow(exception);
+
+        System.out.println(exceptionValue);
+    }
+}

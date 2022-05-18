4e9bcbf08226f0a8c6f4cd35267b5d18a990a334
Tomas Balaz <tomasba@amdocs.com>
2022-05-18 12:53:33 +0200

    Example of *match functions in Stream

--- src/main/java/streams/_StreamBasic.java
+++ src/main/java/streams/_StreamBasic.java
@@ -1,17 +1,16 @@
 package streams;
 
 import java.util.List;
-import java.util.Set;
 import java.util.function.Function;
 import java.util.function.IntConsumer;
+import java.util.function.Predicate;
 import java.util.function.ToIntFunction;
-import java.util.stream.Collectors;
 
-import static streams._Stream.GENDER.FEMALE;
-import static streams._Stream.GENDER.MALE;
+import static streams._StreamBasic.GENDER.FEMALE;
+import static streams._StreamBasic.GENDER.MALE;
 
 
-public class _Stream {
+public class _StreamBasic {
 
     public static void main(String[] args) {
         List<Person> people = List.of(
@@ -30,6 +29,19 @@ public class _Stream {
                 .map(personStringFunction)
                 .mapToInt(length)
                 .forEach(println);
+
+        Predicate<Person> femalePredicate = person -> FEMALE.equals(person.gender);
+        boolean containsOnlyFemales = people.stream()
+                .allMatch(femalePredicate);
+        System.out.println(containsOnlyFemales);
+
+        boolean containsOneFemale = people.stream()
+                .anyMatch(femalePredicate);
+        System.out.println(containsOneFemale);
+
+        boolean containsNoneFemale = people.stream()
+                .noneMatch(femalePredicate);
+        System.out.println(containsNoneFemale);
     }
 
     public static class Person {
